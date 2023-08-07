38a7c21506ed7e4b09cbcb2e112dbaf9dd2443e6
Denis Kaydunov <deniskaydunov@gmail.com>
2023-08-07 14:04:19 +0300

    added the draft of TenantService

--- src/main/java/com/example/javatutorial/controller/CallbackController.java
+++ src/main/java/com/example/javatutorial/controller/CallbackController.java
@@ -1,27 +1,36 @@
 package com.example.javatutorial.controller;
 
+import com.example.javatutorial.service.TenantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "api/v1/callback/tenant")
 public class CallbackController {
 
-  @PutMapping("/callback/v1.0/tenants/*")
-  public String putCallback(@RequestBody RequestBody requestBody) {
-    String tenantAppURL = generateTenantAppURL(requestBody);
-    return tenantAppURL;
+  private final TenantService tenantService;
+
+  @Autowired
+  public CallbackController(TenantService tenantService) {
+    this.tenantService = tenantService;
   }
 
-  @DeleteMapping("/callback/v1.0/tenants/*")
-  public String deleteCallback(@RequestBody RequestBody requestBody) {
-    String tenantAppURL = generateTenantAppURL(requestBody);
-    return tenantAppURL;
+  @PutMapping("/{tenantId}")
+  public ResponseEntity<String> subscribeTenant(@PathVariable(value = "tenantId") String tenantId) {
+    tenantService.subscribe(tenantId);
+    return ResponseEntity.ok(generateTenantURL(tenantId));
   }
 
-  private String generateTenantAppURL(RequestBody requestBody) {
-    String consumerSubdomain = "your_subdomain"; // замените "your_subdomain" на соответствующую логику получения поддомена
+  @DeleteMapping("/{tenantId}")
+  public ResponseEntity<String> unsubscribeTenant(@PathVariable(value = "tenantId") String tenantId) {
+    tenantService.unsubscribe(tenantId);
+    return ResponseEntity.ok(generateTenantURL(tenantId));
+  }
+
+  private String generateTenantURL(String tenantId) {
     //todo replace on variable for "approuter-helloworld.cfapps.us10-001.hana.ondemand.com"
-    String tenantAppURL = "https://" + consumerSubdomain + "-approuter-helloworld.cfapps.us10-001.hana.ondemand.com";
-    return tenantAppURL;
+    return "https://" + tenantId + "-approuter-helloworld.cfapps.us10-001.hana.ondemand.com";
   }
 
 }
