bdbf9f8d538435463ead2cfb0ba7872a9424f261
Denis Kaydunov <deniskaydunov@gmail.com>
2023-08-10 09:16:26 +0300

    added DTO

---
+++ src/main/java/com/github/kaydunovdenis/dto/ReportDto.java
@@ -0,0 +1,21 @@
+package com.github.kaydunovdenis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportDto {
+
+    private Long id;
+    private String description;
+    private LocalDateTime createdAt;
+    private Long employeeId;
+    private Long projectId;
+}
