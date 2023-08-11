37424ea642369bb423b22fa65550c0e6bdca1f3b
Denis Kaydunov <deniskaydunov@gmail.com>
2023-08-11 20:24:15 +0300

    fixed a mistake in the root

--- src/main/java/com/github/kaydunovdenis/config/hibernate/HibernateConfig.java
+++ src/main/java/com/github/kaydunovdenis/config/hibernate/HibernateConfig.java
@@ -1,11 +1,16 @@
 package com.github.kaydunovdenis.config.hibernate;
 
 
+import static org.hibernate.cfg.Environment.FORMAT_SQL;
+import static org.hibernate.cfg.Environment.MULTI_TENANT;
+import static org.hibernate.cfg.Environment.MULTI_TENANT_CONNECTION_PROVIDER;
+import static org.hibernate.cfg.Environment.MULTI_TENANT_IDENTIFIER_RESOLVER;
+
+import com.github.kaydunovdenis.HelloWorldApplication;
 import java.util.HashMap;
 import java.util.Map;
 import javax.sql.DataSource;
 import org.hibernate.MultiTenancyStrategy;
-import org.hibernate.cfg.Environment;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
 import org.springframework.context.annotation.Bean;
@@ -28,14 +33,14 @@ public class HibernateConfig {
                                                                      CurrentTenantIdentifierResolver tenantIdentifierResolver) {
     final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
     em.setDataSource(dataSource);
-    em.setPackagesToScan("com.github.kaydunovdenis.model");
+    em.setPackagesToScan(HelloWorldApplication.class.getPackageName());
     em.setJpaVendorAdapter(this.jpaVendorAdapter());
 
     final Map<String, Object> jpaProperties = new HashMap<>();
-    jpaProperties.put(Environment.MULTI_TENANT, MultiTenancyStrategy.SCHEMA);
-    jpaProperties.put(Environment.MULTI_TENANT_CONNECTION_PROVIDER, multiTenantConnectionProvider);
-    jpaProperties.put(Environment.MULTI_TENANT_IDENTIFIER_RESOLVER, tenantIdentifierResolver);
-    jpaProperties.put(Environment.FORMAT_SQL, true);
+    jpaProperties.put(MULTI_TENANT, MultiTenancyStrategy.SCHEMA);
+    jpaProperties.put(MULTI_TENANT_CONNECTION_PROVIDER, multiTenantConnectionProvider);
+    jpaProperties.put(MULTI_TENANT_IDENTIFIER_RESOLVER, tenantIdentifierResolver);
+    jpaProperties.put(FORMAT_SQL, true);
 
     em.setJpaPropertyMap(jpaProperties);
     return em;
