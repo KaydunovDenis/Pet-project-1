7b4c2960745a8d6097d720505d00644b56920b82
Denis Kaydunov <deniskaydunov@gmail.com>
2023-08-17 17:05:38 +0300

    changed rout

--- src/main/java/com/github/kaydunovdenis/controller/HelloController.java
+++ src/main/java/com/github/kaydunovdenis/controller/HelloController.java
@@ -1,24 +1,19 @@
 package com.github.kaydunovdenis.controller;
 
-import com.github.kaydunovdenis.exception.NotAuthorizedException;
+import com.sap.cloud.security.xsuaa.token.Token;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import com.sap.cloud.security.xsuaa.token.Token;
 
 @RestController
-@RequestMapping(path = "/helloworld")
-public class MainController {
+@RequestMapping(path = "helloworld")
+public class HelloController {
 
   @GetMapping(path = "")
   public ResponseEntity<String> readAll(@AuthenticationPrincipal Token token) {
-    if (!token.getAuthorities().contains(new SimpleGrantedAuthority("Display"))) {
-      throw new NotAuthorizedException("This operation requires \"Display\" scope");
-    }
     return new ResponseEntity<>("Hello Denis Kaydunov!", HttpStatus.OK);
   }
 
